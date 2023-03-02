917c2bc723554e9bde940f9039b9c1c2faa6c3b8
Giansandi025 <109684038+Giansandi025@users.noreply.github.com>
2023-03-02 07:53:40 +0700

    Delete status.js

--- src/routes/status.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { statusController } = require("../controller/status");
-
-router.post("/add-status", statusController.insertStatus);
-module.exports = router;
